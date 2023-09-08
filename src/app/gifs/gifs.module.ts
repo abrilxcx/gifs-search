7916c83b1be3cf3a2fed6624dd1a2241bdf03f17
abrilxcx <abrlbjrn@gmail.com>
2023-09-07 23:38:49 -0600

    gifs components creation

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { ListComponent } from './components/list/list.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    SearchBoxComponent,
+    ListComponent,
   ],
   imports: [
     CommonModule
