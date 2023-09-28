3f11bb0a1eabf22dd9197a468307b4d9b5a0db7a
abrilxcx <abrlbjrn@gmail.com>
2023-09-27 20:52:48 -0600

    searchbox update

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
-  template: `
-    <input type="text"
-    class="form-control mb-3"
-    placeholder="buscar gifs...">
-  `
+  templateUrl: './search-box.component.html'
 })
 export class SearchBoxComponent {
 
